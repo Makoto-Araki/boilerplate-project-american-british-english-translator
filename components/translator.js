af56557c6561f72bb9c6f5ca454e12f23960f812
MakotoAraki <85997616+Makoto-Araki@users.noreply.github.com>
2023-01-25 03:31:41 +0000

    2023年01月25日 - 01

--- components/translator.js
+++ components/translator.js
@@ -19,12 +19,16 @@ class Translator {
       americanToBritishTitles,
       britishOnly ];
     if (locale === 'american-to-british') {
+      console.log(`AAA`);
       let tmp1 = [];
       let tmp2 = [];
       let tmp3 = [];
+      let reg1 = '';
+      let reg2 = '';
       for (let i = 0; i < arr.length; i++) {
         for (let key in arr[i]) {
-          let reg1 = new RegExp(key, 'i');
+          reg1 = new RegExp(key, 'i');
+          reg2 = /[0-9]{2}:[0-9]{2}/;
           if (reg1.test(this.text) === true) {
             tmp1.push(key.length);
             tmp2.push(key);
@@ -32,17 +36,58 @@ class Translator {
           }
         }
       }
+      console.log(`BBB`);
+      console.log(`B01 : ${tmp1.length}`);
+      console.log(`B02 : ${tmp2.length}`);
+      console.log(`B03 : ${tmp3.length}`);
       let wrk1 = 0;
       let wrk2 = tmp1[wrk1];
+      let reg3 = '';
+      let strs = '';
       for (let i = 1; i < tmp1.length; i++) {
         if (wrk2 <= tmp1[i]) {
           wrk1 = i;
           wrk2 = tmp1[wrk1];
         }
       }
-      let reg2 = new RegExp(tmp2[wrk1], 'i');
-      let str1 = '<span class="highlight">' + tmp3[wrk1] + '</span>';
-      this.text = this.text.replace(reg2, str1);
+      console.log(`CCC`);
+      console.log(`C01 : ${wrk1}`);
+      console.log(`C02 : ${tmp2[wrk1]}`);
+      console.log(`C03 : ${tmp3[wrk1]}`);
+      switch(tmp2[wrk1]) {
+        case 'mr.':
+          reg3 = /(mr)(.)/i;
+          strs = '<span class="highlight">$1</span>';
+          break;
+        case 'mrs.':
+          reg3 = /(mrs)(.)/i;
+          strs = '<span class="highlight">$1</span>';
+          break;
+        case 'ms.':
+          reg3 = /(ms)(.)/i;
+          strs = '<span class="highlight">$1</span>';
+          break;
+        case 'mx.':
+          reg3 = /(mx)(.)/i;
+          strs = '<span class="highlight">$1</span>';
+          break;
+        case 'dr.':
+          reg3 = /(dr)(.)/i;
+          strs = '<span class="highlight">$1</span>';
+          break;
+        case 'prof.':
+          reg3 = /(prof)(.)/i;
+          strs = '<span class="highlight">$1</span>';
+          break;
+        default:
+          reg3 = new RegExp(tmp2[wrk1], 'i');
+          strs = '<span class="highlight">' + tmp3[wrk1] + '</span>';
+          break;
+      }
+      console.log(`DDD`);
+      console.log(`BEF : ${this.text}`);
+      this.text = this.text.replace(reg3, strs);
+      console.log(`AFT : ${this.text}`);
     } else {
       //
     }
