1bdd3becc739051332e18a0def55f3e37d736bea
MakotoAraki <85997616+Makoto-Araki@users.noreply.github.com>
2023-01-28 09:36:52 +0000

    2023年01月28日 - 03

--- components/translator.js
+++ components/translator.js
@@ -38,14 +38,14 @@ class Translator {
 
     if (locale === 'american-to-british') {
       // Step1
-      console.log(`STEP1`);
-      console.log(`TEXT : ${this.text}`);
+      //console.log(`STEP1`);
+      //console.log(`TEXT : ${this.text}`);
       reg1 = /([0-9]{1,2}):([0-9]{2})/g;
       exp1 = '<span class="highlight">$1.$2</span>';
       this.text = this.text.replace(reg1, exp1);
       // Step2
-      console.log(`STEP2`);
-      console.log(`TEXT : ${this.text}`);
+      //console.log(`STEP2`);
+      //console.log(`TEXT : ${this.text}`);
       for (let key in americanToBritishTitles) {
         switch (key) {
           case 'mr.':
@@ -81,8 +81,8 @@ class Translator {
         }
       }
       // Step3
-      console.log(`STEP3`);
-      console.log(`TEXT : ${this.text}`);
+      //console.log(`STEP3`);
+      //console.log(`TEXT : ${this.text}`);
       for (let key in this.candidate1) {
         reg3 = new RegExp(key, 'i');
         if (reg3.test(this.text) === true) {
@@ -92,8 +92,8 @@ class Translator {
         }
       }
       // Step4
-      console.log(`STEP4`);
-      console.log(`TEXT : ${this.text}`);
+      //console.log(`STEP4`);
+      //console.log(`TEXT : ${this.text}`);
       let wrk1 = 0;
       let wrk2 = tmp1[wrk1];
       for (let i = 0; i < tmp1.length; i++) {
@@ -103,88 +103,19 @@ class Translator {
         }
       }
       // Step5
-      console.log(`STEP5`);
-      console.log(`TEXT : ${this.text}`);
+      //console.log(`STEP5`);
+      //console.log(`TEXT : ${this.text}`);
       if (tmp2[wrk1] !== undefined) {
         reg3 = new RegExp(tmp2[wrk1], 'ig');
         exp3 = '<span class="highlight">' + tmp3[wrk1] + '</span>';
         this.text = this.text.replace(reg3, exp3);
       }
       // Step6
-      console.log(`Step6`);
-      console.log(`TEXT : ${this.text}`);
+      //console.log(`Step6`);
+      //console.log(`TEXT : ${this.text}`);
 
     } else {
-      /*
-        // Step1
-        reg1 = /([0-9]{1,2}).([0-9]{2})/;
-        exp1 = '<span class="highlight">$1:$2</span>';
-        if (reg1.test(this.text) === true) {
-          this.text = this.text.replace(reg1, exp1);
-        }
-        // Step2
-        for (let j = 0; j < arr.length; j++) {
-          for (let key in arr[j]) {
-            if (arr[j] !== britishOnly) {
-              reg2 = new RegExp(arr[j][key], 'i');
-              if (reg2.test(this.text) === true) {
-                tmp1.push(arr[j][key].length);
-                tmp2.push(arr[j][key]);
-                tmp3.push(key);
-              }
-            } else {
-              reg2 = new RegExp(key, 'i');
-              if (reg2.test(this.text) === true) {
-                tmp1.push(key.length);
-                tmp2.push(key);
-                tmp3.push(arr[j][key]);
-              }
-            }
-          }
-        }
-        wrk1 = 0;
-        wrk2 = tmp1[wrk1];
-        for (let j = 1; j < tmp1.length; j++) {
-          if (wrk2 <= tmp1[j]) {
-            wrk1 = j;
-            wrk2 = tmp1[wrk1];
-          }
-        }
-        if (tmp2[wrk1] !== undefined) {
-          switch(tmp2[wrk1]) {
-            case 'mr':
-              reg2 = /(mr)/i;
-              exp2 = '<span class="highlight">$1.</span>';
-              break;
-            case 'mrs':
-              reg2 = /(mrs)/i;
-              exp2 = '<span class="highlight">$1.</span>';
-              break;
-            case 'ms':
-              reg2 = /(ms)/i;
-              exp2 = '<span class="highlight">$1.</span>';
-              break;
-            case 'mx':
-              reg2 = /(mx)/i;
-              exp2 = '<span class="highlight">$1.</span>';
-              break;
-            case 'dr':
-              reg2 = /(dr)/i;
-              exp2 = '<span class="highlight">$1.</span>';
-              break;
-            case 'prof':
-              reg2 = /(prof)/i;
-              exp2 = '<span class="highlight">$1.</span>';
-              break;
-            default:
-              reg2 = new RegExp(tmp2[wrk1], 'i');
-              exp2 = '<span class="highlight">' + tmp3[wrk1] + '</span>';
-              break;
-          }
-          this.text = this.text.replace(reg2, exp2);
-        }
-      //}
-      */
+      //
     }
     return this.text;
   }
--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -35,27 +35,26 @@ suite('Unit Tests', () => {
   // British to American
   const string11 = 'We watched the footie match for a while.';
   const string12 = 'Paracetamol takes up to an hour to work.';
-  const string13 = '';
-  const string14 = '';
-  const string15 = '';
-  const string16 = '';
-  const string17 = '';
-  const string18 = '';
-  const string19 = '';
-  const string20 = '';
+  const string13 = 'First, caramelise the onions.';
+  const string14 = 'I spent the bank holiday at the funfair.';
+  const string15 = 'I had a bicky then went to the chippy.';
+  const string16 = 'I\'ve just got bits and bobs in my bum bag.';
+  const string17 = 'The car boot sale at Boxted Airfield was called off.';
+  const string18 = 'Have you met Mrs Kalyani?';
+  const string19 = 'Prof Joyner of King\'s College, London.';
+  const string20 = 'Tea time is usually around 4 or 4.30.';
   const translate11 = 'We watched the <span class="highlight">soccer</span> match for a while.';
   const translate12 = '<span class="highlight">Tylenol</span> takes up to an hour to work.';
-  const translate13 = '';
-  const translate14 = '';
-  const translate15 = '';
-  const translate16 = '';
-  const translate17 = '';
-  const translate18 = '';
-  const translate19 = '';
-  const translate20 = '';
+  const translate13 = 'First, <span class="highlight">caramelize</span> the onions.';
+  const translate14 = 'I spent the <span class="highlight">public holiday</span> at the <span class="highlight">carnival</span>.';
+  const translate15 = 'I had a <span class="highlight">cookie</span> then went to the <span class="highlight">fish-and-chip shop</span>.';
+  const translate16 = 'I\'ve just got <span class="highlight">odds and ends</span> in my <span class="highlight">fanny pack</span>.';
+  const translate17 = 'The <span class="highlight">swap meet</span> at Boxted Airfield was called off.';
+  const translate18 = 'Have you met <span class="highlight">Mrs.</span> Kalyani?';
+  const translate19 = '<span class="highlight">Prof.</span> Joyner of King\'s College, London.';
+  const translate20 = 'Tea time is usually around 4 or <span class="highlight">4:30</span>.';
 
-  // American => British Test
-  ///*
+  // American to British Test
   test('Translate to British English - 01', () => {
     assert.equal(translator.translate(string01, option1), translate01);
   });
@@ -87,13 +86,37 @@ suite('Unit Tests', () => {
     assert.equal(translator.translate(string10, option1), translate10);
   });
 
-  // British => American Test
+  // British to American Test
   /*
   test('Translate to American English - 01', () => {
-    assert.equal(translator.translate(str11, option2), tran11);
+    assert.equal(translator.translate(string11, option2), translate11);
   });
   test('Translate to American English - 02', () => {
-    assert.equal(translator.translate(str12, option2), tran12);
+    assert.equal(translator.translate(string12, option2), translate12);
+  });
+  test('Translate to American English - 03', () => {
+    assert.equal(translator.translate(string13, option2), translate13);
+  });
+  test('Translate to American English - 04', () => {
+    assert.equal(translator.translate(string14, option2), translate14);
+  });
+  test('Translate to American English - 05', () => {
+    assert.equal(translator.translate(string15, option2), translate15);
+  });
+  test('Translate to American English - 06', () => {
+    assert.equal(translator.translate(string16, option2), translate16);
+  });
+  test('Translate to American English - 07', () => {
+    assert.equal(translator.translate(string17, option2), translate17);
+  });
+  test('Translate to American English - 08', () => {
+    assert.equal(translator.translate(string18, option2), translate18);
+  });
+  test('Translate to American English - 09', () => {
+    assert.equal(translator.translate(string19, option2), translate19);
+  });
+  test('Translate to American English - 10', () => {
+    assert.equal(translator.translate(string20, option2), translate20);
   });
   */
 });
